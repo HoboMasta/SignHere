06fc583a852f1c6fdbb6bf92def305acf492c275
Cameron Reid <cmreid09@gmail.com>
2016-02-09 19:44:46 -0600

    added lotsa views and controllers

---
+++ SignHere/Controllers/ClientController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingEnvironmentWebApp.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Notaries()
+        {
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+            return View();
+        }
+
+        public ActionResult ProximitySearch()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignHere/Controllers/ReportsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingEnvironmentWebApp.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Accounting()
+        {
+            return View();
+        }
+
+        public ActionResult EmailLog()
+        {
+            return View();
+        }
+    }
+}
